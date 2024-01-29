38347ba8de8f417f69f719d43800677e1c90c847
Thomas Porter <ortjkdev@gmail.com>
2024-01-29 12:33:15 -0330

    Random x positions for obstacles

--- Assets/_Scripts/Navigation/ObstacleSystem.cs
+++ Assets/_Scripts/Navigation/ObstacleSystem.cs
@@ -4,25 +4,38 @@ using UnityEngine;
 
 public class ObstacleSystem : MonoBehaviour
 {
+    [Header("External References")]
     public ShipControls shipControls;
-
+    public LevelTimer levelTimer;
     public List<Obstacle> obstaclePrefabs;
+
+    [Header("Stats")] 
+    public float minDepthToSpawn = 50f;
+    public float xBound = 225f;
+
+    private float _lastSpawnedDepth = 0f;
 
     private void CreateNewObstacle()
     {
         int i = Random.Range(0, obstaclePrefabs.Count);
         GameObject obj = Instantiate(this.obstaclePrefabs[i].gameObject, this.transform);
         obj.GetComponent<Obstacle>().shipControls = this.shipControls;
-        obj.transform.Translate(20f, 0f, 180f);
+
+        float x = Random.Range(-this.xBound, this.xBound);
+        obj.transform.Translate(x, 0f, 200f);
     }
     
     void Start()
     {
-        this.CreateNewObstacle();
+        
     }
 
     void Update()
     {
-        
+        if (this.levelTimer.currentLevel - this._lastSpawnedDepth >= this.minDepthToSpawn)
+        {
+            this.CreateNewObstacle();
+            this._lastSpawnedDepth = this.levelTimer.currentLevel;
+        }
     }
 }
